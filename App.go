eebd868a77c49005d856b9e5d480e0135dc08d2d
pboyle5h <pauricboylegti@gmail.com>
2016-10-10 20:37:24 +0100

    Connection to mongoDB with Manual Add in users

---
+++ App.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/macaron.v1"
+	mgo "gopkg.in/mgo.v2"
+)
+
+func main() {
+	m := macaron.Classic()
+	//getSession()
+
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Run()
+}
+
+type User struct {
+	Username string `json:"username",bson:"username"`
+	Password string `json:"password",bson:"password"`
+}
+
+//adapted from https://stevenwhite.com/building-a-rest-service-with-golang-3/ used to make connection to mongoDB database
+func getSession() *mgo.Session {
+	// Connect to our local mongo
+	s, err := mgo.Dial("mongodb://test:test@ds035006.mlab.com:35006/heroku_lzbj5rj0")
+
+	// Check if connection error, is mongo running?
+	if err != nil {
+		panic(err)
+	}
+	c := s.DB("heroku_lzbj5rj0").C("Users")
+	err = c.Insert(&User{"Ale", "+55 53 8116 9639"},
+		&User{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return s
+}
