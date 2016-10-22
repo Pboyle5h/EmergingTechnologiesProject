6bea0a93ce8ecea8f6602ce0830726cdfaa41bb3
pboyle5h <pauricboylegti@gmail.com>
2016-10-22 14:28:10 +0100

    Added external css style sheet

--- App.go
+++ App.go
@@ -1,53 +1,50 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
-	"net/http"
+    "fmt"
+    "io"
+    "log"
+    "net/http"
 
-	"gopkg.in/macaron.v1"
-	"gopkg.in/mgo.v2"
+    "gopkg.in/macaron.v1"
+    "gopkg.in/mgo.v2"
 )
 
 func main() {
-	m := macaron.Classic()
-	//insert()
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		key := "stuff"
-		val := req.FormValue(key)
-		fmt.Println("Value: " + val)
-		res.Header().Set("Content-Type", "text/html")
-		io.WriteString(res, `<form method="POST"><input type="text" name="stuff"><input type="submit"></form>`)
+m: = macaron.Classic()
+    //insert()
 
-	})
+})
 
-	m.Run()
+m.Run()
 }
 
-type (
-	User struct {
-		Username string
-		Password string
-	}
+type(
+    User struct {
+        Username string
+        Password string
+    }
 )
 
 //adapted from https://stevenwhite.com/building-a-rest-service-with-golang-3/ used to make connection to mongoDB database
 
-func insert() *mgo.Session {
-	// Connect to our local mongo
-	s, err := mgo.Dial("mongodb://test:test@ds035006.mlab.com:35006/heroku_lzbj5rj0")
+func insert() * mgo.Session {
+    // Connect to our local mongo
+    s, err: = mgo.Dial("mongodb://test:test@ds035006.mlab.com:35006/heroku_lzbj5rj0")
 
-	// Check if connection error, is mongo running?
-	if err != nil {
-		panic(err)
-	}
-	c := s.DB("heroku_lzbj5rj0").C("Users")
-	err = c.Insert(&User{"Ale", "+55 53 8116 9639"},
-		&User{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Check if connection error, is mongo running?
+    if err != nil {
+        panic(err)
+    }
+    c: = s.DB("heroku_lzbj5rj0").C("Users")
+    err = c.Insert( & User {
+        "Ale", "+55 53 8116 9639"
+    }, & User {
+        "Cla", "+55 53 8402 8510"
+    })
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	return s
+    return s
 }
